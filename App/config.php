837a8e4054fc0b27ca571b8f01db263f4d62fc94
Amar OUALI <ouali-amar@laposte.net>
2015-06-20 10:51:47 +0000

    Ajoute App/config.php pour la DB

---
+++ App/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'db' => array(
+        'mysql' => array(
+            'host' => 'localhost',
+            'dbname' => 'clients',
+            'login' => 'root',
+            'password' => 'root'
+        )
+    )
+);
